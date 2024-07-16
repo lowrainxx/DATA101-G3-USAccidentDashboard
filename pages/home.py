22c6e352759e8e8e0d7eefb91196c28705bbd92b
EZEE_DT <ezekiel_ducut@dlsu.edu.ph>
2024-07-17 02:25:45 +0800

    home.py - imported logging
    logging helps in distributing the 1.5gb data into "chunks" which prevents bottlenecks and checking where the code might have gotten stuck

--- pages/home.py
+++ pages/home.py
@@ -1,8 +1,13 @@
+# home.py
 import dash
 from dash import dcc, html, register_page
 import plotly.express as px
 import pandas as pd
 import os
+import logging
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
 
 # Register Home page
 register_page(__name__, path='/')
@@ -10,15 +15,24 @@ register_page(__name__, path='/')
 current_directory = os.path.dirname(__file__)
 csv_file_path = os.path.join(current_directory, '..', 'dataset', 'us_accidents_cut.csv')
 
-# Dataset
-try:
-    # df = pd.read_csv('../dataset/us_accidents_clean.csv')
-    df = pd.read_csv(csv_file_path)
-    if df.empty:
-        raise ValueError("The CSV file is empty")
-except Exception as e:
-    raise RuntimeError(f"Error loading CSV file: {e}")
-# df = px.data.iris() # Sample only
+# Load dataset in chunks
+def load_data(file_path):
+    logging.info("Loading data in chunks...")
+    chunk_size = 100000  # Adjust chunk size based on memory availability
+    chunks = []
+    try:
+        for chunk in pd.read_csv(file_path, chunksize=chunk_size, engine='python'):
+            chunks.append(chunk)
+        df = pd.concat(chunks, ignore_index=True)
+        if df.empty:
+            raise ValueError("The CSV file is empty")
+        logging.info("Data loaded successfully.")
+        return df
+    except Exception as e:
+        logging.error(f"Error loading CSV file: {e}")
+        raise RuntimeError(f"Error loading CSV file: {e}")
+
+df = load_data(csv_file_path)
 
 # Figures
 # Chloromap
@@ -30,26 +44,10 @@ severity_counts.columns = ['Severity', 'count']
 
 barsample = px.bar(severity_counts, x='Severity', y='count', title='Accidents by Severity')
 
-# chloromap = px.choropleth(df,
-#                     locations='State',
-#                     locationmode="USA-states",
-#                     color='Severity',
-#                     scope="usa",
-#                     title="Chloropleth Map",
-#                     labels={'accident_count': 'Number of Accidents'},
-#                     color_continuous_scale="Viridis")
-
-# treemap = px.treemap(df,
-#                  path=['State', 'City'],
-#                  values='value',
-#                  title='Treemap')
-
 # Define the layout for the home page
 layout = html.Div([
     html.H1('Home Page'),
     html.Div(children=[
         dcc.Graph(id='severity-bar-chart', figure=barsample)
-        # dcc.Graph(id='graph-1', figure=chloromap, style={'flex': 1})
-        # dcc.Graph(id='graph-1', figure=treemap, style={'flex': 1})
     ], style={'display': 'flex', 'flex-wrap': 'wrap'})
 ])
