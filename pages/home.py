c24aa815373c94cbc7a9e169ad47a8881b1dff2b
Thea1210 <althea_arceta@dlsu.edu.ph>
2024-07-25 18:53:18 +0800

    Added Line Graph

--- pages/home.py
+++ pages/home.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import dask.dataframe as dd
 import os
 import logging
+from dash.dependencies import Input, Output
 
 # Setup logging
 logging.basicConfig(level=logging.INFO)
@@ -13,7 +14,7 @@ logging.basicConfig(level=logging.INFO)
 register_page(__name__, path='/')
 
 current_directory = os.path.dirname(__file__)
-csv_file_path = os.path.join(current_directory, '..', 'dataset', 'us_accidents_cut.csv')
+csv_file_path = os.path.join(current_directory, '..', 'dataset', 'us_accidents_clean.csv')
 
 # Load dataset using Dask
 def load_data(file_path):
@@ -33,6 +34,17 @@ df = load_data(csv_file_path)
 
 # Figures
 # Chloromap
+byhour = df['Hour'].value_counts().reset_index()
+byhour.columns = ['Hour', 'Count']
+bymonth = df['Month'].value_counts().reset_index()
+bymonth.columns = ['Month', 'Count']
+byday = df['Day'].value_counts().reset_index()
+byday.columns = ['Day', 'Count']
+
+byhour = byhour.sort_values(by="Hour")
+bymonth = bymonth.sort_values(by="Month")
+byday = byday.sort_values(by="Day")
+
 accident_counts = df['State'].value_counts().reset_index()
 accident_counts.columns = ['State', 'accident_count']
 
@@ -46,5 +58,34 @@ layout = html.Div([
     html.H1('Home Page'),
     html.Div(children=[
         dcc.Graph(id='severity-bar-chart', figure=barsample)
-    ], style={'display': 'flex', 'flex-wrap': 'wrap'})
+    ], style={'display': 'flex', 'flex-wrap': 'wrap', 'width': '48%', 'margin': '0 auto'}),
+    
+    html.H2('Number of Accidents over Time'),
+    dcc.RadioItems(
+        id='radioitems',
+        options=[
+            {'label': 'Hour', 'value': 'Hour'},
+            {'label': 'DayOfTheMonth', 'value': 'DayOfTheMonth'},
+            {'label': 'Monthly', 'value': 'Monthly'}
+        ],
+        value='DayOfTheMonth',
+        inline=True
+    ),
+    html.Div(children=[
+        dcc.Graph(id='graph')
+    ], style={'display': 'flex', 'flex-wrap': 'wrap', 'width': '48%', 'margin': '0 auto'})
 ])
+
+# Callback to update graph
+@dash.callback(
+    Output('graph', 'figure'),
+    Input('radioitems', 'value')
+)
+def update_graph(selected_option):
+    if selected_option == 'Hour':
+        fig = px.line(byhour, x='Hour', y='Count', title='Accidents by Hour')
+    elif selected_option == 'DayOfTheMonth':
+        fig = px.line(byday, x='Day', y='Count', title='Accidents by Day of the Month')
+    elif selected_option == 'Monthly':
+        fig = px.line(bymonth, x='Month', y='Count', title='Accidents by Month')
+    return fig
